e8c626e70425fefedd5cbda3169f801da8f7d1f4
NGURE-TIM <kangushtim@gmail.com>
2023-09-24 19:15:46 +0300

    Game settings

---
+++ Desktop/python/Project 1/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    '''A class to store all settings'''
+
+    def __init__(self):
+        '''Initialize the game settings'''
+        self.screen_width=1200
+        self.screen_height=800
+        
