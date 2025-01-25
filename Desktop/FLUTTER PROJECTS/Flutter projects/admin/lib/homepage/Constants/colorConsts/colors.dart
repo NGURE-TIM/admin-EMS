1b6fe240fb2a238ba289455f9f411ceaa97e878c
TIM NGURE KARANJA <kangushtim@gmail.com>
2025-01-25 11:30:27 +0300

    Delete Desktop directory

--- Desktop/FLUTTER PROJECTS/Flutter projects/admin/lib/homepage/Constants/colorConsts/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color white=Colors.white;
-const Color black=Colors.black;
-const Color dividerColor=Colors.blueGrey;
-const Color backGround=Colors.orangeAccent;
